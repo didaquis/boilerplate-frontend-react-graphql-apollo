5ad8d83d0bd01aa0939e10a1e95f43bb17a79f7c
Dídac García <didaquis@gmail.com>
2019-11-23 12:30:29 +0100

    Remove comments

--- src/containers/GetListOfUsers.js
+++ src/containers/GetListOfUsers.js
@@ -22,25 +22,3 @@ export const GetListOfUsers = () => (
 		}
 	</Query>
 )
-
-// ++++++++++++++++++++++++++++++++++++++++++++++++++
-
-// Esta implementación tiene dos problemas: polling constante no deseado y que en algunas situaciones al renderizar los datos la ventana hace un scroll hasta el inicio del componente
-
-// import React from 'react'
-// import { useQuery } from 'react-apollo'
-
-// import { Spinner } from '../components/Spinner'
-// import { ErrorAlert } from '../components/ErrorAlert'
-// import { ListOfUsers } from '../components/ListOfUsers'
-
-// import { LIST_ALL_USERS } from '../gql/queries/users';
-
-// export const GetListOfUsers = () => {
-// 	const { loading, error, data } = useQuery(LIST_ALL_USERS);
-
-// 	if (loading) return <Spinner />
-// 	if (error) return <ErrorAlert errorMessage={error.message} />
-
-// 	return <ListOfUsers users={data.listAllUsers || []} />
-// }
