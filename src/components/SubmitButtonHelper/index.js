321132b8de9596ea912e087e1c08608dd104ff82
Dídac García <didaquis@gmail.com>
2019-12-20 20:12:48 +0100

    Remove console.log. Ups! ;-)

--- src/components/SubmitButtonHelper/index.js
+++ src/components/SubmitButtonHelper/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 export const SubmitButtonHelper = ( { mustShowHelper } ) => {
-	console.log(mustShowHelper)
 	return (mustShowHelper) ? <small id="submitHelp" className="form-text text-muted">Submit is disabled until valid data is entered in the form</small> : null
 }
 
