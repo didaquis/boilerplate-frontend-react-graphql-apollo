fc928c68bdd56e1011c9524d8532ae2454b50006
Dídac García <didaquis@gmail.com>
2020-04-29 19:26:45 +0200

    Fixed minor issue of security

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,7 @@ export const Footer = () => {
 		<footer className="footer mt-auto py-3 fixed-bottom bg-dark border-top border-info">
 			<div className="container text-center">
 				<span className="text-muted font-weight-light">
-					Made by <a className="text-info" href="https://didaquis.github.io/">didaquis</a>
+					Made by <a className="text-info" target="_blank" rel="noreferrer noopener" href="https://didaquis.github.io/">didaquis</a>
 				</span>
 			</div>
 		</footer>
