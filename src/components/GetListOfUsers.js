9d2fd13bb6d8592e8f6a4e10be7b8933b8e2262a
Dídac García <didaquis@gmail.com>
2020-12-03 00:03:07 +0100

    Add a polling interval on GetListOfUsers query

--- src/components/GetListOfUsers.js
+++ src/components/GetListOfUsers.js
@@ -8,7 +8,9 @@ import { ListOfUsers } from './ListOfUsers'
 import { LIST_ALL_USERS } from '../gql/queries/users'
 
 export const GetListOfUsers = () => {
-	const { loading, error, data } = useQuery(LIST_ALL_USERS, { fetchPolicy: 'no-cache' });
+	const minuteInMilliseconds = 60000
+	const tenMinutes = minuteInMilliseconds * 10
+	const { loading, error, data } = useQuery(LIST_ALL_USERS, { fetchPolicy: 'no-cache', pollInterval: tenMinutes });
 
 	if (loading) return <Spinner />
 	if (error) return <ErrorAlert errorMessage={error.message} />
