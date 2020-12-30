ad595a0811a138a4045b871349ad1e5bdb969645
Dídac García <didaquis@gmail.com>
2020-12-31 00:10:08 +0100

    Added some test

---
+++ src/components/ErrorAlert/index.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+import { ErrorAlert } from './';
+
+describe('ErrorAlert', () => {
+	it('renders correctly', () => {
+		const { getByRole, getByText } = render(<ErrorAlert errorMessage='foo' />);
+
+		expect(getByRole('alert')).toBeInTheDocument();
+		expect(getByText('foo'));
+	});
+});
---
+++ src/components/LoginForm/index.test.js
@@ -0,0 +1,113 @@
+import React from 'react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { MockedProvider } from '@apollo/client/testing';
+
+import { LOGIN } from '../../gql/mutations/auth';
+import { LoginForm } from './';
+
+describe('LoginForm', () => {
+	it('should render a disabled button until password and email inputs are filled with data', () => {
+		const activateAuth = jest.fn();
+		const mocks = [];
+
+		render(
+			<MockedProvider mocks={mocks} addTypename={false}>
+				<LoginForm activateAuth={activateAuth}/>
+			</MockedProvider>
+		);
+		const emailInput = screen.getByRole('textbox', { name: /Email/i });
+		const passwordInput = screen.getByPlaceholderText(/password/);
+		const submitButton = screen.getByRole('button', { name: 'Log in' });
+		
+		expect(emailInput.value).toBe('');
+		expect(passwordInput.value).toBe('');
+		expect(submitButton).toBeDisabled();
+
+		fireEvent.change(emailInput, { target: { value: 'example@mail.com' } });
+		expect(submitButton).toBeDisabled();
+
+		fireEvent.change(passwordInput, { target: { value: 'ABCabc*1234*4321' } });
+		expect(submitButton).not.toBeDisabled();
+
+		fireEvent.change(emailInput, { target: { value: '' } });
+		fireEvent.change(passwordInput, { target: { value: '' } });
+		expect(submitButton).toBeDisabled();
+	});
+
+	it('should call to activateAuth method passing a token as argument if credentials are valid', async () => {
+		const activateAuth = jest.fn();
+		const mocks = [
+			{
+				request: {
+					query: LOGIN,
+					variables: {
+						email: 'example@mail.com',
+						password: 'ABCabc*1234*4321',
+					},
+				},
+				result: {
+					data: {
+						authUser: {
+							token: 'f3b2c1a0d2'
+						}
+					},
+				},
+			},
+		];
+
+		render(
+			<MockedProvider mocks={mocks} addTypename={false}>
+				<LoginForm activateAuth={activateAuth}/>
+			</MockedProvider>
+		);
+
+		const emailInput = screen.getByRole('textbox', { name: /Email/i });
+		const passwordInput = screen.getByPlaceholderText(/password/);
+		const submitButton = screen.getByRole('button', { name: 'Log in' });
+
+		fireEvent.change(emailInput, { target: { value: 'example@mail.com' } });
+		fireEvent.change(passwordInput, { target: { value: 'ABCabc*1234*4321' } });
+		fireEvent.click(submitButton);
+
+		await waitFor(() => expect(activateAuth).toHaveBeenCalled());
+		expect(activateAuth).toHaveBeenCalledWith('f3b2c1a0d2');
+	});
+
+	it('should render an error if credentials are not valid', async () => {
+		const activateAuth = jest.fn();
+		const mocks = [
+			{
+				request: {
+					query: LOGIN,
+					variables: {
+						email: 'example@mail.com',
+						password: 'ABCabc*1234*4321',
+					},
+				},
+				result: {
+					errors: [new Error('Invalid credentials')],
+				},
+			},
+		];
+
+		render(
+			<MockedProvider mocks={mocks} addTypename={false}>
+				<LoginForm activateAuth={activateAuth}/>
+			</MockedProvider>
+		);
+
+		const emailInput = screen.getByRole('textbox', { name: /Email/i });
+		const passwordInput = screen.getByPlaceholderText(/password/);
+		const submitButton = screen.getByRole('button', { name: 'Log in' });
+
+		fireEvent.change(emailInput, { target: { value: 'example@mail.com' } });
+		fireEvent.change(passwordInput, { target: { value: 'ABCabc*1234*4321' } });
+		fireEvent.click(submitButton);
+
+		await waitFor(() => expect(activateAuth).not.toHaveBeenCalled());
+
+		expect(screen.getByRole('alert')).toBeInTheDocument();
+		expect(screen.getByText('Invalid credentials'));
+	});
+});
